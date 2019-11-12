2c6f7d6376da81ca243a1695f1ca98b003fe7ed2
Takaki <takaki55730317@gmail.com>
2019-11-12 15:51:14 +0900

    taskテーブルのname カラムに30文字制限のlimit設定

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_051237) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
