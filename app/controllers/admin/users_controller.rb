84970024c0e3bc27b863349b4a177581524db2e4
Takaki <takaki55730317@gmail.com>
2019-11-13 18:09:43 +0900

    管理者用usersコントローラのアクション設定及びユーザー新規登録ビューの作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,47 @@
 class Admin::UsersController < ApplicationController
-  def new
-  end
-
-  def edit
+  
+  def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
-  def index
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render "new"
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}を更新しました。"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}を削除しました。"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 end
