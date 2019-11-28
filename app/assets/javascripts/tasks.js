9b215cda8f2f8fc935d75730978e8904b6ebf895
Takaki <takaki55730317@gmail.com>
2019-11-28 14:49:16 +0900

    コントローラからJavaScriptを返して実行(SJR)

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -8,11 +8,13 @@ document.addEventListener('turbolinks:load', function() {
       e.currentTarget.style.backgroundColor = '';
     });
   });
-  document.querySelectorAll('.delete').forEach(function(a) {
-    a.addEventListener('ajax:success', function() {
-      var td = a.parentNode;
-      var tr = td.parentNode;
-      tr.style.display = 'none';
-    });
-  });
+  // コントローラからJavaScriptを返して実行する』で不要になるためにコメントアウトする.
+  
+  // document.querySelectorAll('.delete').forEach(function(a) {
+  //   a.addEventListener('ajax:success', function() {
+  //     var td = a.parentNode;
+  //     var tr = td.parentNode;
+  //     tr.style.display = 'none';
+  //   });
+  // });
 });
